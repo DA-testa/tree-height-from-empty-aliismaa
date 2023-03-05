56e71b5f6e27a9bc654c8187409fc555b2fe1872
Alisa Mamikova <116195263+aliismaa@users.noreply.github.com>
2023-03-06 01:05:14 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,4 +1,3 @@
-# python3
 import sys
 import threading
 
@@ -7,50 +6,43 @@ def compute_height(n, parents):
     for i in range(n):
         tree[i] = []
     for i in range(n):
-        parent = parents[i]
-        if parent == -1:
+        if parents[i] == -1:
             root = i
         else:
-            tree[parent].append(i)
+            tree[parents[i]].append(i)
 
     def comp_height(node):
         if not tree[node]:
             return 1
         max_height = 0
-        for ch in tree[node]:
-            height = comp_height(ch)
+        for ch in children[node]:
+            depth = compute_depth(ch)
             max_height = max(max_height, height)
         return max_height + 1
-    
-    return comp_height(root)
+
+    return compute_depth(root)
 
 def main():
+
     choice = input()
     if "I" in choice:
         name = int(input())
         parents = list(map(int, input().split()))
-        print(compute_height(name, parents))
-
     elif "F" in choice:
         path = './test/'
-        filename = input("Enter the file name: ")
-        
-        if "a" in filename:
-            print("File names with letter a are not allowed")
+        filename = input("Enter file name: ")
+        if 'a' in file_name:
+            print("File contains letter 'a'")
             return
-        try:
-          with open(path + filename, 'r', encoding='utf-8') as file:
-            name = int(file.readline())
-            parents = list(map(int, file.readline().split()))
-            print(compute_height(name, parents))
-        except FileNotFoundError:
-            print("Error: File not found")
-            return
+        with open(path + filename, 'r') as file:
+                n = int(file.readline())
+                parents = list(map(int, file.readline().split()))
     else:
         print("Error")
-
+        return 
+       
     print(compute_height(name, parents))
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**27) # new thread will get stack of such size
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
-main()
