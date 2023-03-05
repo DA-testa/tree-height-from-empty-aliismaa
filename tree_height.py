090fe8703d92dc554c35b765157e19e3b0eb1797
Alisa Mamikova <116195263+aliismaa@users.noreply.github.com>
2023-03-06 01:16:27 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,48 +1,50 @@
-#python3
+# python3
 import sys
 import threading
 
 def compute_height(n, parents):
     tree = {}
-     for i in range(n):
+    for i in range(n):
         parent = parents[i]
-        if parent == -1:
-            root = i
-        else:
-            tree[parent].append(i)
+    if parent == -1:
+        root = i
+    else:
+        tree[parent].append(i)
 
     def comp_height(node):
-        if not tree[node]:
-            return 1
-        max_height = 0
-        for ch in tree[node]:
-            height = compute_height(ch)
-            max_height = max(max_height, height)
-        return max_height + 1
+    if not tree[node]:
+        return 1
+    max_height = 0
+    for ch in tree[node]:
+        height = comp_height(ch)
+        max_height = max(max_height, height)
+    return max_height + 1
 
-    return compute_height(root)
+    return comp_height(root)
 
 def main():
-
     choice = input()
     if "I" in choice:
         name = int(input())
         parents = list(map(int, input().split()))
+        print(compute_height(name, parents))
+
     elif "F" in choice:
-        path = './test/'
-        filename = input("Enter file name: ")
-        if 'a' in file_name:
-            print("File contains letter 'a'")
-            return
-        with open(path + filename, 'r') as file:
-                n = int(file.readline())
-                parents = list(map(int, file.readline().split()))
+        print("Enter the file name: ")
+        FileName = input()
+    if "a" in FileName:
+        print("File names with letter a are not allowed")
+        return
+    with open("test/" + FileName, ) as file:
+        name = int(file.readline())
+        parents = list(map(int, file.readline().split()))
+        print(compute_height(name, parents))
+
     else:
         print("Error")
-        return 
-       
-    print(compute_height(name, parents))
+        exit()
 
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
 threading.Thread(target=main).start()
+main()
